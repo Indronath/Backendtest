f8354e1a6af6ea9f3ac03fb005370bec32c6f853
Indronath <coolindro007@gmail.com>
2024-05-13 16:14:21 +0530

    MyData Added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000;
+const mydata = {
+  "Name": "Indronath das",
+  "Age":33
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,6 +15,10 @@ app.get('/facebook',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.send("<h1>Welcome to login page</h1>")
 })
+app.get('/mydata',(req,res)=>{
+  res.json(mydata);
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
